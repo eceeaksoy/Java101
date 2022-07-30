f6b55f6e1ee00a0da5695c2b7bca20798a9959a6
Sabiha Ece Aksoy <108814142+eceeaksoy@users.noreply.github.com>
2022-07-31 01:25:20 +0300

    Add files via upload

---
+++ Alistirma/UsluSayiBulma.java
@@ -0,0 +1,23 @@
+package Alistirma;
+
+import java.util.Scanner;
+
+public class UsluSayiBulma {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int n, k, tot = 1;
+        System.out.print("Ussu alinacak sayi: ");
+        n = keyboard.nextInt();
+        System.out.print("Us olacak sayi: ");
+        k = keyboard.nextInt();
+
+        int i = 1;
+        while (i <= k)
+        {
+            tot *= n;
+            i++;
+        }
+
+        System.out.println(n + "^" + k + " = " + tot);
+    }
+}
